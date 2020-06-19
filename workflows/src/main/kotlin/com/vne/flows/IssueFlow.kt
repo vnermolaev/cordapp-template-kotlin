848790ca4417d107c1d7f1c9fbd90006e75c9a90
Victor Ermolaev <victorermolaev@gmail.com>
2020-06-19 14:02:31 +0200

    Add IssueFlow

---
+++ workflows/src/main/kotlin/com/vne/flows/IssueFlow.kt
@@ -0,0 +1,98 @@
+package com.vne.flows
+
+import co.paralleluniverse.fibers.Suspendable
+import com.vne.contracts.TokenContract
+import com.vne.states.TokenState
+import net.corda.core.contracts.Command
+import net.corda.core.flows.*
+import net.corda.core.identity.Party
+import net.corda.core.node.StatesToRecord
+import net.corda.core.transactions.SignedTransaction
+import net.corda.core.transactions.TransactionBuilder
+import net.corda.core.utilities.ProgressTracker
+
+object IssueFlow {
+    data class Acquirer(val party: Party, val quantity: Long)
+
+    @InitiatingFlow
+    @StartableByRPC
+    class Initiator(acquirers: List<Acquirer>) : FlowLogic<SignedTransaction>() {
+        private val acquirers: List<Acquirer>
+
+        // This breaks everything.
+        // private val notary = serviceHub.networkMapCache.notaryIdentities.first()
+
+        init {
+            require(acquirers.isNotEmpty()) { "List of acquirers must not be empty "}
+            // Flow will fail at state creation if any quantity is less or equal to 0.
+
+            this.acquirers = acquirers
+        }
+
+        companion object {
+            object GENERATING_TRANSACTION : ProgressTracker.Step("Generating transaction.")
+            object VERIFYING_TRANSACTION : ProgressTracker.Step("Verifying contract constraints.")
+            object SIGNING_TRANSACTION : ProgressTracker.Step("Issuer signs transaction.")
+            object FINALISING_TRANSACTION : ProgressTracker.Step("Obtaining notary signature and recording transaction.") {
+                override fun childProgressTracker() = FinalityFlow.tracker()
+            }
+
+            fun tracker() = ProgressTracker(
+                GENERATING_TRANSACTION,
+                VERIFYING_TRANSACTION,
+                SIGNING_TRANSACTION,
+                FINALISING_TRANSACTION
+            )
+        }
+
+        override val progressTracker = tracker()
+
+        @Suspendable
+        override fun call(): SignedTransaction {
+            val issuer = ourIdentity
+
+            // Generate.
+            progressTracker.currentStep = GENERATING_TRANSACTION
+
+            val notary = serviceHub.networkMapCache.notaryIdentities.first()
+            val txBuilder = TransactionBuilder(notary)
+                .addCommand(Command(TokenContract.Commands.Issue(), issuer.owningKey))
+            acquirers.map { txBuilder.addOutputState(TokenState(issuer, it.party, it.quantity)) }
+
+            // Verify.
+            progressTracker.currentStep = VERIFYING_TRANSACTION
+            txBuilder.verify(serviceHub)
+
+            // Sign.
+            progressTracker.currentStep = SIGNING_TRANSACTION
+            // Only issuer needs to sign. Holder will only accept.
+            val fullySignedTx = serviceHub.signInitialTransaction(txBuilder)
+
+            // Finalize.
+            progressTracker.currentStep = FINALISING_TRANSACTION
+            val counterPartiesFlows = acquirers.map { it.party }
+                .distinct()
+                .minus(issuer)
+                .map { initiateFlow(it) }
+
+
+            return subFlow(FinalityFlow(
+                fullySignedTx,
+
+                // This breaks everything
+                //counterParties.map { initiateFlow(it) },
+
+                counterPartiesFlows,
+                FINALISING_TRANSACTION.childProgressTracker()
+            ))
+        }
+    }
+
+    @InitiatedBy(Initiator::class)
+    class Responder(val counterpartySession: FlowSession) : FlowLogic<SignedTransaction>() {
+        @Suspendable
+        override fun call(): SignedTransaction {
+            return subFlow(ReceiveFinalityFlow(counterpartySession))
+        }
+    }
+}
--- workflows/src/test/kotlin/com/vne/FlowTests.kt
+++ workflows/src/test/kotlin/com/vne/FlowTests.kt
@@ -1,6 +1,8 @@
 package com.vne
 
-import com.vne.flows.Responder
+import com.vne.flows.IssueFlow
+import net.corda.core.utilities.getOrThrow
+import net.corda.testing.core.singleIdentity
 import net.corda.testing.node.MockNetwork
 import net.corda.testing.node.MockNetworkParameters
 import net.corda.testing.node.TestCordapp
@@ -9,18 +11,27 @@ import org.junit.Before
 import org.junit.Test
 
 class FlowTests {
-    private val network = MockNetwork(MockNetworkParameters(cordappsForAllNodes = listOf(
-        TestCordapp.findCordapp("com.template.contracts"),
-        TestCordapp.findCordapp("com.template.flows")
-    )))
-    private val a = network.createNode()
-    private val b = network.createNode()
+    private val network = MockNetwork(
+        MockNetworkParameters(cordappsForAllNodes = listOf(
+            TestCordapp.findCordapp("com.vne.contracts"),
+            TestCordapp.findCordapp("com.vne.flows")
+        )))
+
+    private val airline = network.createNode()
+    private val airlineParty = airline.info.singleIdentity()
+
+    private val alice = network.createNode()
+    private val aliceParty = alice.info.singleIdentity()
+
+    private val bob = network.createNode()
+    private val bobParty = bob.info.singleIdentity()
 
     init {
-        listOf(a, b).forEach {
-            it.registerInitiatedFlow(Responder::class.java)
+        listOf(airline, alice, bob).forEach {
+            it.registerInitiatedFlow(IssueFlow.Responder::class.java)
         }
     }
+
 
     @Before
     fun setup() = network.runNetwork()
@@ -28,8 +39,15 @@ class FlowTests {
     @After
     fun tearDown() = network.stopNodes()
 
-    @Test
-    fun `dummy test`() {
 
+    @Test
+    fun `Valid transaction receives all signatures`() {
+        val acquirer = IssueFlow.Acquirer(aliceParty, 25L)
+        val flow = IssueFlow.Initiator(listOf(acquirer))
+        val fut = airline.startFlow(flow)
+        network.runNetwork()
+
+        val signedTx = fut.getOrThrow()
+        signedTx.verifyRequiredSignatures()
     }
 }
